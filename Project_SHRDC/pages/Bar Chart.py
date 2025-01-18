bf9d53671366fa86ad66bf20f51b4621d0f795b1
awangkuazzam <98402887+awangkuazzam@users.noreply.github.com>
2025-01-18 17:22:00 +0800

    Add files via upload

--- Project_SHRDC/pages/Bar Chart.py
+++ Project_SHRDC/pages/Bar Chart.py
@@ -2,9 +2,6 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
-if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
 
 @st.cache_data
 def load_data():
--- Project_SHRDC/pages/Pie Chart.py
+++ Project_SHRDC/pages/Pie Chart.py
@@ -3,9 +3,6 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
-if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
 
 # Pre-load CSV data (replace 'your_data.csv' with your actual CSV file)
 df = pd.read_csv('dataset.csv')
